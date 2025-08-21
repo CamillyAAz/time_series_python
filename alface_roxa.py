230352aa685f359ec286ef659561d6cbd4a98f73
Camilly <camillyalves0602@gmail.com>
2025-08-21 20:08:45 -0300

    First Commit

---
+++ alface_roxa.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+=====================================================
+Nome do Script : alface_roxa.py
+Descrição      : exemplo de cabeçalho Python
+Autor          : Camilly Alves
+Data           : 21/08/2025
+Versão         : 1.0
+Licença        : MIT (ou outra que preferir)
+=====================================================
+"""
+
+import sys
+import os
+
+def main():
+    print("Ta funcionando!")
+
+if __name__ == "__main__":
+    main()
